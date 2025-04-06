3ededfabb690468f63e389887fddc1be0a516e81
BigflipBOT <flipypr@protonmail.com>
2025-04-06 22:00:13 +0200

    added internet connection status to bar

--- sway/scripts.d/bar_logic.sh
+++ sway/scripts.d/bar_logic.sh
@@ -3,7 +3,8 @@ do
 	date=$(date +'%d/%m/%Y')
 	time=$(date +'%X')
 	batt=$(cat /sys/class/power_supply/BAT1/capacity)"%"
+  conn=$(iwctl station list | head -n +5 | tail -n -1 | awk '{print $2 ": " $3}') # frist station from list
 
-	echo "$batt | $date | $time"
+	echo "$conn | $batt | $date | $time"
 	sleep 1
 done
