57ad9b40944e43009333a81687220ff967481359
BigflipBOT <flipypr@protonmail.com>
2025-04-07 12:15:05 +0200

    modified status bar to support colored output

--- sway/scripts.d/bar_logic.sh
+++ sway/scripts.d/bar_logic.sh
@@ -1,10 +1,32 @@
+color(){
+  text=$1 
+  color_hex=$2
+  echo "<span foreground='${color_hex}'>${text}</span>"
+}
+
+
 while true
 do
+  # set values of custom variables
 	date=$(date +'%d/%m/%Y')
 	time=$(date +'%X')
 	batt=$(cat /sys/class/power_supply/BAT1/capacity)"%"
   conn=$(iwctl station list | head -n +5 | tail -n -1 | awk '{print $2 ": " $3}') # frist station from list
 
-	echo "$conn | $batt | $date | $time"
+  separator=$(color " | " "#511E1E")
+
+  # insert variables in wanted order into the array
+  modules=("$conn" "$batt" "$date" "$time")
+
+  # array print
+  printval=""
+  modules_size=${#modules[@]}
+  for ((i = 0; i < modules_size; i++)); do
+    printval+=$(echo ${modules[$i]})
+    if [[ $i+1 -lt modules_size ]]; then printval+=$separator; fi
+  done
+
+  echo $printval
+
 	sleep 1
 done
