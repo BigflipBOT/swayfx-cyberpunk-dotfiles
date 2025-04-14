195e4cde97eaf0ed2fec3caa42b39ae01009758f
BigflipBOT <flipypr@protonmail.com>
2025-04-15 00:24:17 +0200

    changed swaylock config

--- sway/scripts.d/swaylock.sh
+++ sway/scripts.d/swaylock.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
 swaylock \
+  --daemonize \
+  --tiling \
 	--screenshots \
+  --ignore-empty-password \
 	--clock \
 	--indicator \
-	--indicator-radius 100 \
-	--indicator-thickness 7 \
-	--effect-blur 7x5 \
-	--effect-vignette 0.5:0.5 \
-	--ring-color bb00cc \
-	--key-hl-color 880033 \
+	--indicator-radius 150 \
+	--indicator-thickness 2 \
+	--effect-blur 3x1 \
+	--effect-pixelate 7x5 \
+	--ring-color 511E1E \
+	--key-hl-color F95E5A \
 	--line-color 00000000 \
-	--inside-color 00000088 \
+	--inside-color 0D0D1588 \
+  --text-color F95E5A \
+  --inside-clear-color 27121988 \
+  --text-clear-color F95E5A \
+  --ring-clear-color F95E5A \
+  --line-clear-color F95E5A \
+  --inside-wrong-color fc022888\
+  --line-wrong-color F3445E88 \
+  --inside-ver-color 28E09688 \
+  --line-ver-color 60DFAC88 \
 	--separator-color 00000000 \
-	--grace 2 \
-	--fade-in 0.2
