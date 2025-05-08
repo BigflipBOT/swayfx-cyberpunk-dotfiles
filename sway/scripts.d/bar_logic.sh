20901dc3b2b07d7dd8a91896e5ebf87763cc57df
BigflipBOT <57315276+BigflipBOT@users.noreply.github.com>
2025-05-08 17:37:35 +0000

    Update bar_logic.sh
    cleanup and changed to pactl

--- sway/scripts.d/bar_logic.sh
+++ sway/scripts.d/bar_logic.sh
@@ -7,27 +7,27 @@ color(){
 
 while true
 do
-  # set values of custom variables
+  	# set values of custom variables
 	date=$(date +'%d/%m/%Y')
 	time=$(date +'%X')
 	batt=$(cat /sys/class/power_supply/BAT1/capacity)"%"
-  conn=$(iwctl station list | head -n +5 | tail -n -1 | awk '{print $2 ": " $3}') # frist station from list
-  sound=$(echo "sound: $(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master))")
+	conn=$(iwctl station list | head -n +5 | tail -n -1 | awk '{print $2 ": " $3}') # frist station from list
+	sound=$(echo "sound: $(pactl get-sink-volume @DEFAULT_SINK@ | grep -oE "front-left: [0-9]+ / [ 0-9]+%" | grep -oE "[0-9]+%" )")
 
-  separator=$(color " | " "#511E1E")
+	separator=$(color " | " "#511E1E")
 
-  # insert variables in wanted order into the array
-  modules=("$sound" "$conn" "$batt" "$date" "$time")
+	# insert variables in wanted order into the array
+	modules=("$sound" "$conn" "$batt" "$date" "$time")
 
-  # array print
-  printval=""
-  modules_size=${#modules[@]}
-  for ((i = 0; i < modules_size; i++)); do
-    printval+=$(echo ${modules[$i]})
-    if [[ $i+1 -lt modules_size ]]; then printval+=$separator; fi
-  done
+	# array print
+	printval=""
+	modules_size=${#modules[@]}
+	for ((i = 0; i < modules_size; i++)); do
+		printval+=$(echo ${modules[$i]})
+	if [[ $i+1 -lt modules_size ]]; then printval+=$separator; fi
+	done
 
-  echo $printval
+	echo $printval
 
 	sleep 1
 done
